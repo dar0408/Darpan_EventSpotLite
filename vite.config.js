8e841fa4052049858539d7678e80fbd7b43c8b04
dar0408 <dsalgotra2021@gmail.com>
2024-10-28 00:47:57 +0530

    m

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-icons/fc'], // Prevents bundling of react-icons/fc
+//     },
+//   },
+// });
+
+export default {
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/fc'], // Prevents bundling of react-icons/fc
-    },
-  },
-});
+      external: ['react-icons/Fc']
+    }
+  }
+}
